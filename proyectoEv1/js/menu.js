3d883b2e8a85f2c3037ed7a6f1adc7cb69a730e5
LilMax <max.lorca@duocuc.cl>
2025-09-08 20:49:52 -0300

    Modificacion total del menu

--- proyectoEv1/js/menu.js
+++ proyectoEv1/js/menu.js
@@ -1,8 +1,9 @@
 function openNav() {
-        document.getElementById("sidebar").classList.add("open");
+    document.getElementById("sidebar").style.width = "250px";
+    document.getElementById("overlay").style.display = "block";
     }
-    
+
 function closeNav() {
-        document.getElementById("sidebar").classList.remove("open");
+    document.getElementById("sidebar").style.width = "0";
+    document.getElementById("overlay").style.display = "none";
     }
-    
